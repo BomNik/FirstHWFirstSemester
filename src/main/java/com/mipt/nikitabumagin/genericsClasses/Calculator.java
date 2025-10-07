cff664705916653367f47371052fea515ea2953c
BumaginNikita <bumagin.ns@phystech.edu>
2025-10-07 13:25:35 +0300

    Add Calculator class

---
+++ src/main/java/com/mipt/nikitabumagin/genericsClasses/Calculator.java
@@ -0,0 +1,36 @@
+package com.mipt.nikitabumagin.genericsClasses;
+
+public class Calculator<T extends Number> {
+
+  public double sum(T a, T b) {
+    if (a == null || b == null) {
+      return Double.NaN;
+    }
+
+    return a.doubleValue() + b.doubleValue();
+  }
+
+  public double subtract(T a, T b) {
+    if (a == null || b == null) {
+      return Double.NaN;
+    }
+
+    return a.doubleValue() - b.doubleValue();
+  }
+
+  public double multiply(T a, T b) {
+    if (a == null || b == null) {
+      return Double.NaN;
+    }
+
+    return a.doubleValue() * b.doubleValue();
+  }
+
+  public double divide(T a, T b) {
+    if (a == null || b == null || b.doubleValue() == 0.0) {
+      return Double.NaN;
+    }
+
+    return a.doubleValue() / b.doubleValue();
+  }
+}
