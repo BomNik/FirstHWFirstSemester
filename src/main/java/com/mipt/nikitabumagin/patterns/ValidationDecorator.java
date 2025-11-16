e32a86889d69bd5140bdfbd71ed7ee47c76b6dc7
BumaginNikita <bumagin.ns@phystech.edu>
2025-11-16 23:31:00 +0300

    ValidationDecorator.java

--- src/main/java/com/mipt/nikitabumagin/patterns/ValidationDecorator.java
+++ src/main/java/com/mipt/nikitabumagin/patterns/ValidationDecorator.java
@@ -2,25 +2,42 @@ package com.mipt.nikitabumagin.patterns;
 
 import java.util.Optional;
 
-// TODO: Декоратор для валидации
-//  Должен:
-//      1. При findDataByKey - влидируются входные данные
-//      2. При saveData - влидируются входные данные
-//      3. При deleteData - влидируются входные данные
 public class ValidationDecorator implements DataService {
+
+  private final DataService original;
+
+  public ValidationDecorator(DataService dataService) {
+    this.original = dataService;
+  }
+
+  private void validateKey(String key) {
+    if (key == null || key.trim().isEmpty()) {
+      throw new IllegalArgumentException("Ключ не может быть пустым или null.");
+    }
+  }
+
+  private void validateData(String data) {
+    if (data == null) {
+      throw new IllegalArgumentException("Данные не могут быть null.");
+    }
+  }
 
   @Override
   public Optional<String> findDataByKey(String key) {
-    return Optional.empty();
+    validateKey(key);
+    return original.findDataByKey(key);
   }
 
   @Override
   public void saveData(String key, String data) {
-
+    validateKey(key);
+    validateData(data);
+    original.saveData(key, data);
   }
 
   @Override
   public boolean deleteData(String key) {
-    return false;
+    validateKey(key);
+    return original.deleteData(key);
   }
 }
