8fd971e658137b316f769b2ba054cb0cb2806ef7
BumaginNikita <bumagin.ns@phystech.edu>
2025-11-16 23:13:34 +0300

    CachingDecorator.java

--- src/main/java/com/mipt/nikitabumagin/patterns/CachingDecorator.java
+++ src/main/java/com/mipt/nikitabumagin/patterns/CachingDecorator.java
@@ -1,26 +1,41 @@
 package com.mipt.nikitabumagin.patterns;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-// TODO: Декоратор для кеширования
-//  Должен:
-//      1. При findDataByKey - кешировать результаты
-//      2. При saveData - обновлять данные в кэше
-//      3. При deleteData - инвалидировать кэш
 class CachingDecorator implements DataService {
+
+  private final DataService original;
+  private final Map<String, String> cache = new HashMap<>();
+
+  public CachingDecorator(DataService dataService) {
+    this.original = dataService;
+  }
 
   @Override
   public Optional<String> findDataByKey(String key) {
-    return Optional.empty();
+    if (cache.containsKey(key)) {
+      return Optional.of(cache.get(key));
+    }
+
+    Optional<String> result = original.findDataByKey(key);
+    result.ifPresent(data -> cache.put(key, data));
+    return result;
   }
 
   @Override
   public void saveData(String key, String data) {
-
+    original.saveData(key, data);
+    cache.put(key, data);
   }
 
   @Override
   public boolean deleteData(String key) {
-    return false;
+    boolean isDeleted = original.deleteData(key);
+    if (isDeleted) {
+      cache.remove(key);
+    }
+    return isDeleted;
   }
 }
